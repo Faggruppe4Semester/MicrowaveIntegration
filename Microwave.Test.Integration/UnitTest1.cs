36233efbf54e90cedf995e47fb298de658dc71ed
MatiasTvorup <matias@tvorup.eu>
2020-04-22 13:59:46 +0200

    Integrationstestprojekt

---
+++ Microwave.Test.Integration/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microwave.Test.Integration
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
