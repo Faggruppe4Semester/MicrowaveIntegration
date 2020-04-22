47c5b472eb658c4338b94e2e47dd04d4de5a7598
Worly97 <frede0@hotmail.com>
2020-04-22 14:18:28 +0200

    Fixed testclass

---
+++ Microwave.Test.Integration/IT1_CookController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NSubstitute;
+using NSubstitute.Core.Arguments;
+
+namespace Microwave.Test.Integration
+{
+    [TestFixture]
+    class IT1_CookController
+    {
+
+    }
+}
