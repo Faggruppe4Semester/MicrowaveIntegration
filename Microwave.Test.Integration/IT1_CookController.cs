1bdbb183f6b6c11ace1ac5fdec695c819497aabf
Worly97 <frede0@hotmail.com>
2020-04-22 14:25:48 +0200

    RefixedTestFixture

--- Microwave.Test.Integration/IT1_CookController.cs
+++ Microwave.Test.Integration/IT1_CookController.cs
@@ -6,12 +6,25 @@ using System.Threading.Tasks;
 using NUnit.Framework;
 using NSubstitute;
 using NSubstitute.Core.Arguments;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
 
 namespace Microwave.Test.Integration
 {
     [TestFixture]
     class IT1_CookController
     {
+        CookController _uut;
+        IOutput _output;
+        IPowerTube _powerTube;
+        ITimer _timer;
+        IDisplay _display;
 
+
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
     }
 }
